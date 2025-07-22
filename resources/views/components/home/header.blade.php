140726c3581a6d685ce7b55b6da8ae963cfe6f29
“Mohamed <mushkirmohamed@gmail.com>
2025-07-23 01:47:43 +0530

    style: designed custom home layout with tailwind

---
+++ resources/views/components/home/header.blade.php
@@ -0,0 +1,1 @@
+<h1 class=" text-center text-3xl text-white pt-10 font-bold">Yajra Data-table demo</h1>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.home')
+@section('content')
+    <div>
+        <h1>Home Page</h1>
+    </div>
+@endsection
